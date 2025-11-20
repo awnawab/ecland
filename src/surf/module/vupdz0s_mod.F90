ef58f0d8e48bcd067b96710813a660bb28c24967
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2025-11-20 18:40:03 +0100

    Comment out unused include
    (cherry picked from commit 6b746c3e772ae1316a0084c629403cd5b4770fee)

--- src/surf/module/vupdz0s_mod.F90
+++ src/surf/module/vupdz0s_mod.F90
@@ -188,7 +188,7 @@ REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
 !             INCLUDE STABILITY FUNCTIONS
 !             ------- --------- ---------
 
-#include "fcsvdfs.h"
+!#include "fcsvdfs.h"
 
 !             INCLUDE ROUGNESS LENGTH FUNCTIONS
 !             ------- -------- ------ ---------
