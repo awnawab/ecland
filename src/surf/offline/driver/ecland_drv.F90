83c732df1b8f4fece8ae3fe986e9939497ba5049
Zbigniew Piotrowski <22048166+piotrows@users.noreply.github.com>
2026-03-17 13:03:11 +0100

    Nullify pointers
    (cherry picked from commit 736ff950375d723f081986763ae499227c560171)

--- src/surf/offline/driver/ecland_drv.F90
+++ src/surf/offline/driver/ecland_drv.F90
@@ -253,7 +253,6 @@ ENDDO !IST LOOP
 
 
 !     ------------------------------------------------------------------
-!These shall be called at the last timestep only
 !CALL PSURF%FINAL
 !CALL PSURFL%FINAL
 !CALL PDIAG%FINAL
